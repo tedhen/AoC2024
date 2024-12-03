77d143b3a9992fc21f88262e616efd5ac1a41a4d
Ted Henriksson <ted.henriksson@ai.se>
2024-12-03 10:50:00 +0100

    Day3 part2

--- Day3/day3.py
+++ Day3/day3.py
@@ -5,7 +5,7 @@ import re
 
 def main():
     """Lets do som regexp"""
-    with open('./AoC2024/Day3/input.txt', encoding="utf-8") as f:
+    with open('./Day3/input.txt', encoding="utf-8") as f:
         puzzle_input = f.readlines()
         
         # Part 1
@@ -17,6 +17,23 @@ def main():
             for m in matching:
                 n = numbers.findall(m)
                 sum += int(n[0]) * int(n[1])
+        print(sum)
+
+        # Part 2
+        reg = re.compile('mul\(\d{1,3},\d{1,3}\)|don\'t\(\)|do\(\)')
+        numbers = re.compile('\d{1,3}')
+        sum = 0
+        enable = True
+        for l in puzzle_input:
+            matching = reg.findall(l)
+            for m in matching:
+                if 'don' in m:
+                    enable = False
+                elif 'do()' in m: 
+                    enable = True
+                elif enable:
+                    n = numbers.findall(m)
+                    sum += int(n[0]) * int(n[1])
         print(sum)
 
 
